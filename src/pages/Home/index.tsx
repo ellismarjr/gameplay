d8c6301b2d9c0136ee7787ca1d1adddce7e08d5b
Junior de Oliveira <ellismarjr@hotmail.com>
2021-06-23 10:55:34 -0300

    add appointment list

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,72 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { FlatList, View } from 'react-native';
+import { Appointment } from '../../components/Appointment';
+
 import { ButtonAdd } from '../../components/ButtonAdd';
 import { CategorySelect } from '../../components/CategorySelect';
-
+import { ListDivider } from '../../components/ListDivider';
+import { ListHeader } from '../../components/ListHeader';
 import { Profile } from '../../components/Profile';
 
 import { styles } from './styles';
 
 export function Home() {
   const [category, setCategory] = useState('');
+
+  const appointments = [
+    {
+      id: '1',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true,
+      },
+      category: '1',
+      date: '22/06 às 20:40',
+      description:
+        'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
+    },
+    {
+      id: '2',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: false,
+      },
+      category: '1',
+      date: '22/06 às 20:40',
+      description:
+        'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
+    },
+    {
+      id: '3',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: false,
+      },
+      category: '1',
+      date: '22/06 às 20:40',
+      description:
+        'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
+    },
+    {
+      id: '4',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: false,
+      },
+      category: '1',
+      date: '22/06 às 20:40',
+      description:
+        'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
+    },
+  ];
 
   function handleCategorySelect(categoryId: string) {
     setCategory(categoryId === category ? '' : categoryId);
@@ -21,10 +79,21 @@ export function Home() {
         <ButtonAdd />
       </View>
 
-      <View>
-        <CategorySelect
-          categorySelected={category}
-          setCategory={handleCategorySelect}
+      <CategorySelect
+        categorySelected={category}
+        setCategory={handleCategorySelect}
+      />
+
+      <View style={styles.content}>
+        <ListHeader title="Partidas agendadas" subtitle="subtitle" />
+
+        <FlatList
+          data={appointments}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <Appointment data={item} />}
+          contentContainerStyle={styles.matches}
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => <ListDivider />}
         />
       </View>
     </View>
