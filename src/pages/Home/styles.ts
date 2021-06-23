db73f18054dbe2e42f0808dd469ca91ad795deea
Junior de Oliveira <ellismarjr@hotmail.com>
2021-06-23 09:15:03 -0300

    add CategorySelect component

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -11,5 +11,6 @@ export const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginTop: getStatusBarHeight() + 26,
+    marginBottom: 26,
   },
 });
