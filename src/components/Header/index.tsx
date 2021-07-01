fdd602bb337f8ea2a24ded3bbe040b1ac419477a
Junior de Oliveira <ellismarjr@hotmail.com>
2021-07-01 08:41:13 -0300

    add goBack Home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,6 +4,7 @@ import { Text, View } from 'react-native';
 import Feather from '@expo/vector-icons/Feather';
 import { BorderlessButton } from 'react-native-gesture-handler';
 
+import { useNavigation } from '@react-navigation/native';
 import { theme } from '../../global/styles/theme';
 import { styles } from './styles';
 
@@ -13,14 +14,19 @@ type HeaderProps = {
 };
 
 export function Header({ title, action }: HeaderProps) {
+  const navigation = useNavigation();
   const { secondary100, secondary40, heading } = theme.colors;
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
 
   return (
     <LinearGradient
       style={styles.container}
       colors={[secondary100, secondary40]}
     >
-      <BorderlessButton>
+      <BorderlessButton onPress={handleGoBack}>
         <Feather name="arrow-left" size={24} color={heading} />
       </BorderlessButton>
       <Text style={styles.title}>{title}</Text>
