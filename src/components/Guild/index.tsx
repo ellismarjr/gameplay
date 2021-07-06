67b4dd5cc82d4ca9888b0e5c2328a5492e6b15f8
Junior de Oliveira <ellismarjr@hotmail.com>
2021-07-06 10:15:00 -0300

    chore: add Guld component

---
+++ src/components/Guild/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+  Text,
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { GuildIcon } from '../GuildIcon';
+
+import { styles } from './styles';
+import { theme } from '../../global/styles/theme';
+
+type GuildData = {
+  id: string;
+  name: string;
+  icon: string | null;
+  owner: boolean;
+};
+
+type GuildProps = TouchableOpacityProps & {
+  data: GuildData;
+};
+
+export function Guild({ data, ...rest }: GuildProps) {
+  return (
+    <TouchableOpacity activeOpacity={0.7} {...rest} style={styles.container}>
+      <GuildIcon />
+
+      <View style={styles.content}>
+        <View>
+          <Text style={styles.title}>{data.name}</Text>
+
+          <Text style={styles.type}>
+            {data.owner ? 'Administrador' : 'Convdado'}
+          </Text>
+        </View>
+      </View>
+
+      <Feather name="chevrons-right" color={theme.colors.heading} size={24} />
+    </TouchableOpacity>
+  );
+}
