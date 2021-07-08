70a571900c8bda726745fd11c1651dd792e70a81
Junior de Oliveira <ellismarjr@hotmail.com>
2021-07-08 09:49:40 -0300

    fix: Header title centered

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -31,7 +31,7 @@ export function Header({ title, action }: HeaderProps) {
       </BorderlessButton>
       <Text style={styles.title}>{title}</Text>
 
-      {action && <View>{action}</View>}
+      {action ? <View>{action}</View> : <View style={{ width: 24 }} />}
     </LinearGradient>
   );
 }
